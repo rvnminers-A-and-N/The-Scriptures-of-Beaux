c89b4db3f503b29e6ab6e6f56ca00e93c8380f97
rvnminers-A-and-N <rvnminers@gmail.com>
2024-02-13 19:24:55 -0500

    Commit Twenty Eight: Splashing/Packing Scriptures

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow () {
+  var splash = new BrowserWindow({ 
+    width: 500,
+    height: 300,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true
+  });
+  
+  splash.loadFile('resources/splash.html');
+  splash.center();
+
   // Create the browser window.
-  const win = new BrowserWindow({
+  var win = new BrowserWindow({
     width: 800,
     height: 600,
-    fullscreen: true,
+    show: false,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
+  // Maximize the window to take up the entire screen space but still keep the window controls
+  //win.maximize();
+
+  // If you really want it to cover the entire screen but not in the fullscreen mode, you can uncomment the following:
+  // const { screen } = require('electron');
+  // const primaryDisplay = screen.getPrimaryDisplay();
+  // const { width, height } = primaryDisplay.workAreaSize;
+  // win.setBounds({ x: 0, y: 0, width, height });
+
   // and load the index.html of the app.
-  win.loadFile('resources/The Scriptures of Beaux.html');
+  // win.loadFile('resources/The Scriptures of Beaux.html');
 
   // Open the DevTools.
   // win.webContents.openDevTools()
-}
+
+  // Hide the menu bar
+  //win.setMenuBarVisibility(false);
+  // Or to remove the menu entirely
+  // win.setMenu(null);
+
+  setTimeout(function () {
+    splash.close();
+    win.center();
+    win.maximize();
+    win.loadFile('resources/The Scriptures of Beaux.html');
+    win.show();
+  }, 5000);
+
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
 
-// Quit when all windows are closed, except on macOS. There,
-// it's common for applications to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  // Quit when all windows are closed, except on macOS. There,
+  // it's common for applications to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 });
